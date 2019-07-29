2548f7907e767fef8a7f6fb164669e041b6437d4
Jesus <aliass.jr1@gmail.com>
2019-07-29 16:25:44 -0400

    current score for players set at 0

--- app.js
+++ app.js
@@ -18,12 +18,27 @@ activePlayer = 1;
 dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('.btn-roll').addEventListener('click')
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // random number
+   var dice = Math.floor(Math.random() * 6) + 1;
+
+   //display result
+   var diceDOM = document.querySelector('.dice');
+   diceDOM.style.display = 'block';
+   diceDOM.src = 'dice-'
+ + dice + '.png';
+})
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
+
